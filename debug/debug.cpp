59569123a81c661377a3ddf768019cb5bec645b7
Suzukaze <1027743497@qq.com>
2024-02-17 18:48:27 +0800

    update debug

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -1,85 +1,166 @@
 #pragma once
 #include<iostream>
 #include<sstream>
-#include<concepts>
 
-#define debug(...) suzukaze::_debug(__LINE__, #__VA_ARGS__ __VA_OPT__(,) __VA_ARGS__)
+std::ostream &operator<<(std::ostream &out, __int128_t a) {
+    if (!a) return out << 0;
+    if (a < 0) {
+        out << '-';
+        a = -a;
+    }
+    static int b[40], bn;
+    for (bn = 0; a; a /= 10)
+        b[++bn] = a % 10;
+    for (int i = bn; i > 0; --i)
+        out << b[i];
+    return out;
+}
 
-namespace suzukaze
-{
-    template<typename T> concept string = std::convertible_to<T, std::string>;
-    template<typename T> concept iterable = requires(const T & t) { std::begin(t); std::end(t); };
-    template<typename T> concept has_top = requires(T t) { t.top(); };
+#define debug(...) suzukaze::Debug(__LINE__, #__VA_ARGS__ __VA_OPT__(,) __VA_ARGS__)
 
-    class _debug
-    {
-        const std::string blank = std::string(1, ' '), sep = "," + blank, equal = blank + '=' + blank;
+namespace suzukaze {
+    constexpr int OUT_LEN = 1;
+    constexpr int IN_LEN = 1;
+    constexpr bool SPLIT = false;
 
+    template<typename T> constexpr bool is_string = std::is_convertible_v<T, std::string>;
+
+    template<typename T> constexpr bool is_tuple = false;
+    template<typename ...T> constexpr bool is_tuple<std::tuple<T...>> = true;
+
+    template<typename T, typename = std::void_t<>> constexpr bool is_iterable = false;
+    template<typename T> constexpr bool is_iterable<T, std::void_t<decltype(std::begin(std::declval<T &>())), decltype(std::end(std::declval<T &>()))>> = true;
+
+#define check(name, ...) \
+    template<typename, typename = std::void_t<>> constexpr bool name = false; \
+    template<typename T> constexpr bool name<T, std::void_t<__VA_ARGS__>> = true;
+
+    check(has_pop, decltype(std::declval<T>().pop()));
+    check(has_top, decltype(std::declval<T>().top()));
+    check(has_front, decltype(std::declval<T>().front()));
+    check(can_cout, decltype(std::cout << std::declval<T>()));
+
+#undef check
+
+    struct Any { template<typename T> operator T(); };
+    template<std::size_t N> struct Tag : Tag<N - 1> {};
+    template<> struct Tag<0> {};
+    template<typename T> constexpr auto size_(Tag<0>) -> decltype(0u) { return 0; }
+    template<typename T> constexpr auto size_(Tag<1>) -> decltype(T{ Any{} }, 0) { return 1; }
+    template<typename T> constexpr auto size_(Tag<2>) -> decltype(T{ Any{}, Any{} }, 0) { return 2; }
+    template<typename T> constexpr auto size_(Tag<3>) -> decltype(T{ Any{}, Any{}, Any{} }, 0) { return 3; }
+    template<typename T> constexpr auto size_(Tag<4>) -> decltype(T{ Any{}, Any{}, Any{}, Any{} }, 0) { return 4; }
+    template<typename T> constexpr auto size_(Tag<5>) -> decltype(T{ Any{}, Any{}, Any{}, Any{}, Any{} }, 0) { return 5; }
+    template<typename T> constexpr auto size_(Tag<6>) -> decltype(T{ Any{}, Any{}, Any{}, Any{}, Any{}, Any{} }, 0) { return 6; }
+    template<typename T> constexpr auto size() {
+        if constexpr (std::is_aggregate_v<T>)
+            return size_<T>(Tag<6>{});
+        else
+            return std::tuple_size_v<T>;
+    }
+
+    class Debug {
+        const std::string in_sep = ',' + std::string(IN_LEN, ' '), out_blank = std::string(OUT_LEN, ' '),
+            out_sep = out_blank + '|' + out_blank, equal = out_blank + '=' + out_blank;
         std::ostringstream sout;
         std::string names;
-        int idx = 0, cover = 0;
+        std::string::size_type idx = 0, cover = 0;
 
     public:
         template<typename T, typename ...U>
-        _debug(const int line, const std::string &names, const T &arg, const U &...args) : names(names)
-        {
+        Debug(const int line, std::string &&names, const T &arg, const U &...args) : names(std::move(names)) {
             sout << ".." << line << "..\t";
-            if constexpr (!iterable<T>)
-                _print(arg, false), (..., _print(args, true));
+            if constexpr ((is_iterable<T> || std::is_pointer_v<T> || has_pop<T> || has_front<T> || has_top<T>) && (std::is_arithmetic_v<U> && ...))
+                print(arg, false, args...);
             else
-                _print(arg, false, args...);
+                print(arg, false), (..., print(args, true));
             std::clog << sout.str() << "\n";
         }
 
-        _debug(const int line, const std::string &names) { std::clog << "-------------------------------\n"; }
+        Debug(const int line, const std::string &names) { std::clog << "-------------------------------\n"; }
 
     private:
-        void _print_sep(int idx) { if (idx) sout << sep; }
+        void print_in_sep(bool flag = true) { if (flag) sout << in_sep; }
 
         template<typename T, typename ...U>
-        void _print(const T &arg, const bool flag, const U &...args)
-        {
-            _print_sep(flag);
-            while (idx < names.size() && (cover || names[idx] != ','))
-            {
-                cover += names[idx] == '(' || names[idx] == '<';
-                cover -= names[idx] == ')' || names[idx] == '>';
-                sout << names[idx++];
-            }
-            sout << equal;
-            idx += sep.size();
+        void print(const T &arg, const bool flag, const U &...args) {
+            if constexpr (SPLIT) {
+                if (flag)
+                    sout << out_sep;
+                while (idx < names.size() && (cover || names[idx] != ',')) {
+                    cover += names[idx] == '(' || names[idx] == '<';
+                    cover -= names[idx] == ')' || names[idx] == '>';
+                    if (names[idx] != ' ')
+                        sout << names[idx];
+                    idx++;
+                }
+                sout << equal;
+                idx++;
+            } else if (!flag)
+                sout << names << equal;
+            else
+                print_in_sep();
 
-            __print(arg, args...);
+            print_(arg, args...);
         }
 
         template<typename T, typename ...U>
-        void __print(const T &arg, const int size = INT_MAX, const U & ...args)
-        {
-            int idx = 0;
+        void print_(const T &arg, const std::size_t len = -1, const U & ...args) {
             if constexpr (std::is_arithmetic_v<T>)
                 sout << arg;
-            else if constexpr (string<T>)
+            else if constexpr (is_string<T>)
                 sout << '"' << arg << '"';
             else if constexpr (std::__is_pair<T>)
-                sout << '(', __print(arg.first, size, args...), sout << sep, __print(arg.second, size, args...), sout << ')';
-            else if constexpr (iterable<T>)
-            {
+                sout << '(', print_(arg.first), print_in_sep(), print_(arg.second), sout << ')';
+            else if constexpr (is_iterable<T>) {
                 sout << '[';
-                for (auto it = std::begin(arg); idx < size && it != std::end(arg); it++, idx++)
-                    _print_sep(idx), __print(*it, args...);
+                auto it = std::begin(arg);
+                for (std::size_t idx = 0; idx < len && it != std::end(arg); it++, idx++)
+                    print_in_sep(idx), print_(*it, args...);
                 sout << ']';
-            }
-            else
-            {
+            } else if constexpr (std::is_pointer_v<T>) {
+                sout << '[';
+                for (std::size_t idx = 0; idx < len; idx++)
+                    print_in_sep(idx), print_(arg[idx], args...);
+                sout << ']';
+            } else if constexpr (has_pop<T>) {
                 auto t = arg;
-                sout << '<';
-                while (idx < size && t.size())
+                sout << '[';
+                for (std::size_t idx = 0; idx < len && t.size(); idx++)
                     if constexpr (has_top<T>)
-                        _print_sep(idx++), __print(t.top()), t.pop();
+                        print_in_sep(idx), print_(t.top(), args...), t.pop();
                     else
-                        _print_sep(idx++), __print(t.front()), t.pop();
-                sout << '>';
-            }
+                        print_in_sep(idx), print_(t.front(), args...), t.pop();
+                sout << ']';
+            } else if constexpr (can_cout<T>)
+                sout << arg;
+            else if constexpr (std::is_aggregate_v<T> || is_tuple<T>) {
+                if constexpr (size<T>() <= 6) {
+                    sout << "{("[is_tuple<T>];
+                    constexpr auto cnt = size<T>();
+                    if constexpr (cnt == 1) {
+                        const auto &[m1] = arg;
+                        print_(m1);
+                    } else if constexpr (cnt == 2) {
+                        const auto &[m1, m2] = arg;
+                        print_(m1), print_in_sep(), print_(m2);
+                    } else if constexpr (cnt == 3) {
+                        const auto &[m1, m2, m3] = arg;
+                        print_(m1), print_in_sep(), print_(m2), print_in_sep(), print_(m3);
+                    } else if constexpr (cnt == 4) {
+                        const auto &[m1, m2, m3, m4] = arg;
+                        print_(m1), print_in_sep(), print_(m2), print_in_sep(), print_(m3), print_in_sep(), print_(m4);
+                    } else if constexpr (cnt == 5) {
+                        const auto &[m1, m2, m3, m4, m5] = arg;
+                        print_(m1), print_in_sep(), print_(m2), print_in_sep(), print_(m3), print_in_sep(), print_(m4), print_in_sep(), print_(m5);
+                    } else {
+                        const auto &[m1, m2, m3, m4, m5, m6] = arg;
+                        print_(m1), print_in_sep(), print_(m2), print_in_sep(), print_(m3), print_in_sep(), print_(m4), print_in_sep(), print_(m5), print_in_sep(), print_(m6);
+                    }
+                    sout << "})"[is_tuple<T>];
+                }
+            } else
+                sout << "NoDebugType";
         }
     };
 }
